c46f7699af96049bec2bdae6dde2cf9ba53ec617
titanknis <titanknis@gmail.com>
2025-05-08 07:55:28 +0100

    format: reformatted files for readablity

--- Semestre2/Cours-ASD-en-C/LinkedList/LL-bidirectionnelle-code-complet.c
+++ Semestre2/Cours-ASD-en-C/LinkedList/LL-bidirectionnelle-code-complet.c
@@ -3,9 +3,9 @@
 /*****Liste bidirectionnelle*/
 struct cellule
 {
-    struct cellule *avant;
-    int info;
-    struct cellule *suivant;
+    struct cellule* avant;
+    int             info;
+    struct cellule* suivant;
 };
 struct liste
 {
@@ -13,249 +13,243 @@ struct liste
     struct cellule* dernier;
 };
 /******Fonction creer_liste*******/
-void creer_liste(struct liste *L)
+void creer_liste(struct liste* L)
 {
-    L->premier=NULL ;
-    L->dernier=NULL ;
+    L->premier = NULL;
+    L->dernier = NULL;
 }
 /******Fonction tester_liste_vide*******/
 int tester_liste_vide(struct liste L)
 {
-    return L.premier==NULL;
+    return L.premier == NULL;
 }
 /******Fonction Ajout inserer_avant_premier *******/
-void inserer_avant_premier(struct liste *L,int x)
+void inserer_avant_premier(struct liste* L, int x)
 {
-    struct cellule * p;
-    p=(struct cellule*)malloc(sizeof(struct cellule));
-    p->info=x;
-    p->suivant=L->premier;
-    p->avant=NULL;
-    if(L->premier==NULL)
-        L->dernier=p;
+    struct cellule* p;
+    p          = (struct cellule*)malloc(sizeof(struct cellule));
+    p->info    = x;
+    p->suivant = L->premier;
+    p->avant   = NULL;
+    if (L->premier == NULL)
+        L->dernier = p;
     else
-        (L->premier)->avant=p;
-    L->premier=p;
+        (L->premier)->avant = p;
+    L->premier = p;
 }
 /**Fonction Remplir 1 en utilisant inserer_avant_premier**/
-void remplir1(struct liste * L , int n)
+void remplir1(struct liste* L, int n)
 {
     int i, x;
-    for(i=1 ; i<=n ; i++)
+    for (i = 1; i <= n; i++)
     {
         printf("Donner un entier : ");
-        scanf("%d",&x);
-        inserer_avant_premier(L , x);
+        scanf("%d", &x);
+        inserer_avant_premier(L, x);
     }
 }
 /******Fonction Ajout inserer_aprés_dernier *******/
-void inserer_apres_dernier(struct liste *L,int x)
+void inserer_apres_dernier(struct liste* L, int x)
 {
-    struct cellule * p;
-    p=(struct cellule*)malloc(sizeof(struct cellule));
-    p->info=x;
-    p->avant=L->dernier;
-    p->suivant=NULL;
-    if(L->dernier==NULL)
-        L->premier=p;
+    struct cellule* p;
+    p          = (struct cellule*)malloc(sizeof(struct cellule));
+    p->info    = x;
+    p->avant   = L->dernier;
+    p->suivant = NULL;
+    if (L->dernier == NULL)
+        L->premier = p;
     else
-        (L->dernier)->suivant=p;
-    L->dernier=p;
+        (L->dernier)->suivant = p;
+    L->dernier = p;
 }
 /**Fonction Remplir 2 en utilisant inserer_avant_premier**/
-void remplir2(struct liste * L , int n)
+void remplir2(struct liste* L, int n)
 {
     int i, x;
-    for(i=1 ; i<=n ; i++)
+    for (i = 1; i <= n; i++)
     {
         printf("Donner un entier : ");
-        scanf("%d",&x);
-        inserer_apres_dernier(L , x);
+        scanf("%d", &x);
+        inserer_apres_dernier(L, x);
     }
 }
 /****Fonction TAILLE***/
 int taille(struct liste L)
 {
-    struct cellule * p;
-    unsigned t=0;
-    if(tester_liste_vide(L))
-        return(0);
+    struct cellule* p;
+    unsigned        t = 0;
+    if (tester_liste_vide(L))
+        return (0);
     else
     {
-        p=L.premier;
-        while(p!=NULL)
+        p = L.premier;
+        while (p != NULL)
         {
-            p=p->suivant;
+            p = p->suivant;
             t++;
         }
-        return(t);
+        return (t);
     }
 }
 /**** Fonction recherche_référence****/
- struct cellule * recherche_reference(struct liste L , int x)
- {
-     struct cellule * p=L.premier;
-     while (p)
-        if(p->info==x)
+struct cellule* recherche_reference(struct liste L, int x)
+{
+    struct cellule* p = L.premier;
+    while (p)
+        if (p->info == x)
             return p;
         else
-            p=p->suivant;
+            p = p->suivant;
     return NULL;
 }
 /****Fonction insertion après élément référencée*****/
-void inserer_apres_reference(struct cellule * p, int x)
+void inserer_apres_reference(struct cellule* p, int x)
 {
-    struct cellule *q;
-    q = (struct cellule*)malloc(sizeof(struct cellule));
-    q->info = x;
-    q->suivant = p->suivant ;
-    q->avant=p;
-    p->suivant->avant=q;
-    p->suivant=q;
+    struct cellule* q;
+    q                 = (struct cellule*)malloc(sizeof(struct cellule));
+    q->info           = x;
+    q->suivant        = p->suivant;
+    q->avant          = p;
+    p->suivant->avant = q;
+    p->suivant        = q;
 }
 /****Fonction insertion après élément référencée*****/
-void inserer_avant_reference(struct cellule * p, int x)
+void inserer_avant_reference(struct cellule* p, int x)
 {
-    struct cellule *q;
-    q = (struct cellule*)malloc(sizeof(struct cellule));
-    q->info = x;
-    q->suivant = p ;
-    q->avant=p->avant;
-    p->avant->suivant=q;
-    p->avant=q;
+    struct cellule* q;
+    q                 = (struct cellule*)malloc(sizeof(struct cellule));
+    q->info           = x;
+    q->suivant        = p;
+    q->avant          = p->avant;
+    p->avant->suivant = q;
+    p->avant          = q;
 }
 /****Fonction supprimer_premier*****/
-void supprimer_premier(struct liste *L)
+void supprimer_premier(struct liste* L)
 {
-    struct cellule * p;
+    struct cellule* p;
     if (tester_liste_vide(*L))
-        printf ("Liste vide\n" );
+        printf("Liste vide\n");
+    else if (L->premier == L->dernier)
+    {
+        free(L->premier);
+        L->premier = NULL;
+        L->dernier = NULL;
+    }
     else
-        if (L->premier==L->dernier)
-        {
-            free(L->premier);
-            L->premier=NULL;
-            L->dernier=NULL;
-        }
-        else
-        {
-            p=L->premier;
-            (p->suivant)->avant=NULL;
-            L->premier=p->suivant;
-            free(p);
-        }
+    {
+        p                   = L->premier;
+        (p->suivant)->avant = NULL;
+        L->premier          = p->suivant;
+        free(p);
+    }
 }
 /****Fonction supprimer_dernier*****/
-void supprimer_dernier(struct liste *L)
+void supprimer_dernier(struct liste* L)
 {
-    struct cellule * p;
+    struct cellule* p;
     if (tester_liste_vide(*L))
-        printf ("Liste vide\n" );
+        printf("Liste vide\n");
+    else if (L->premier == L->dernier)
+    {
+        free(L->premier);
+        L->premier = NULL;
+        L->dernier = NULL;
+    }
     else
-        if (L->premier==L->dernier)
-        {
-            free(L->premier);
-            L->premier=NULL;
-            L->dernier=NULL;
-        }
-        else
-        {
-            p=L->dernier;
-            (p->avant)->suivant=NULL;
-            L->dernier=p->avant;
-            free(p);
-        }
+    {
+        p                   = L->dernier;
+        (p->avant)->suivant = NULL;
+        L->dernier          = p->avant;
+        free(p);
+    }
 }
 /*****suppression d’un élément référencée******/
-void supprimer_element_reference( struct cellule *p)
+void supprimer_element_reference(struct cellule* p)
 {
-    p->avant->suivant=p->suivant;
-    p->suivant->avant=p->avant;
+    p->avant->suivant = p->suivant;
+    p->suivant->avant = p->avant;
     free(p);
 }
 /*****Fonction supprimer_apres_reference******/
-void supprimer_apres_reference(struct cellule * p)
+void supprimer_apres_reference(struct cellule* p)
 {
-    struct cellule * q=p->suivant;
-    p->suivant=q->suivant;
-    q->suivant->avant=p;
+    struct cellule* q = p->suivant;
+    p->suivant        = q->suivant;
+    q->suivant->avant = p;
     free(q);
 }
 /*****Fonction supprimer_avant_reference******/
-void supprimer_avant_reference(struct cellule * p)
+void supprimer_avant_reference(struct cellule* p)
 {
-    struct cellule * q=p->avant;
-    p->avant=q->avant;
-    q->avant->suivant=p;
+    struct cellule* q = p->avant;
+    p->avant          = q->avant;
+    q->avant->suivant = p;
     free(q);
 }
 /****Fonction Affiche*****/
 void affiche(struct liste L)
 {
-    struct cellule * p ;
+    struct cellule* p;
     if (tester_liste_vide(L))
-        printf ("Liste vide\n" );
+        printf("Liste vide\n");
+    else if (L.premier == L.dernier)
+        printf("La liste à un seul élément : %d ", L.premier->info);
     else
-        if (L.premier == L.dernier)
-            printf ("La liste à un seul élément : %d " ,L.premier->info);
-        else
+    {
+        p = L.premier; /* p= L.queue : de gauche à droite */
+        while (p)
         {
-            p=L.premier;    /* p= L.queue : de gauche à droite */
-            while (p)
-            {
-                printf ("%d\t",p->info);
-                p= p->suivant;
-            }
-         }
+            printf("%d\t", p->info);
+            p = p->suivant;
+        }
+    }
     printf("\n");
 }
 /****Fonction Affiche*****/
 void affiche_adresse(struct liste L)
 {
-    struct cellule * p ;
+    struct cellule* p;
 
-    p=L.premier;    /* p= L.queue : de gauche à droite */
-    printf ("%X\t",L.premier);
+    p = L.premier; /* p= L.queue : de gauche à droite */
+    printf("%X\t", L.premier);
     while (p)
     {
-        printf ("%X\t",p->suivant);
-        p= p->suivant;
+        printf("%X\t", p->suivant);
+        p = p->suivant;
     }
     printf("\n");
 }
 void INVERSE_LISTE(struct liste L)
 {
-    int aux;
-    struct cellule *  p1=L.premier;
-    struct cellule * p2=L.dernier;
-    while(p1!=p2 && p1->avant!=p2)
+    int             aux;
+    struct cellule* p1 = L.premier;
+    struct cellule* p2 = L.dernier;
+    while (p1 != p2 && p1->avant != p2)
     {
-        aux =p1->info;
-        p1->info=p2->info;
-        p2->info=aux;
-        p1=p1->suivant;
-        p2=p2->avant;
+        aux      = p1->info;
+        p1->info = p2->info;
+        p2->info = aux;
+        p1       = p1->suivant;
+        p2       = p2->avant;
     }
 }
 int main()
 {
-    struct liste L1;
-    int n =6,x,y;
-    struct cellule * p;
-    creer_liste(&L1) ;
+    struct liste    L1;
+    int             n = 6, x, y;
+    struct cellule* p;
+    creer_liste(&L1);
     /*remplir1(&L1,n);
     printf("***Contenu de L1***\n");
     affiche(L1);*/
     printf("****Chargement de L1****\n");
-    remplir2(&L1,n);
+    remplir2(&L1, n);
     printf("***Contenu de L1***\n");
     affiche(L1);
     INVERSE_LISTE(L1);
     printf("***Contenu de L1***\n");
     affiche(L1);
-
-
-
 
     /*****inserer_avant_premier *****/
     /*printf("****inserer_avant_premier ****\n",130,130,130);
@@ -320,9 +314,8 @@ int main()
     affiche(L1);*/
 
     /*****suppression d’un élément référencé******/
-    /*printf("****suppression d'un %cl%cment %c partir d'une r%cf%crence donn%c****\n",130,130,133,130,130,130);
-    printf("Donner un entier : ");
-    scanf("%d",&x);
+    /*printf("****suppression d'un %cl%cment %c partir d'une r%cf%crence
+    donn%c****\n",130,130,133,130,130,130); printf("Donner un entier : "); scanf("%d",&x);
     p=recherche_reference(L1,x);
     if(p)
     {
@@ -334,10 +327,9 @@ int main()
     else
         printf("La r%cf%crence de %d n'existe pas dans la liste\n",130,130,x);*/
 
-     /*****suppression d’un élément avant une référence******/
-    /*printf("****suppression d'un %cl%cment avant une r%cf%crence donn%c****\n",130,130,130,130,130);
-    printf("Donner un entier : ");
-    scanf("%d",&x);
+    /*****suppression d’un élément avant une référence******/
+    /*printf("****suppression d'un %cl%cment avant une r%cf%crence
+    donn%c****\n",130,130,130,130,130); printf("Donner un entier : "); scanf("%d",&x);
     p=recherche_reference(L1,x);
     if(p)
     {
@@ -350,9 +342,8 @@ int main()
         printf("La r%cf%crence de %d n'existe pas dans la liste\n",130,130,x);*/
 
     /*****suppression d’un élément apres une référence******/
-    /*printf("****suppression d'un %cl%cment apr%cs une r%cf%crence donn%c****\n",130,130,130,130,130,130);
-    printf("Donner un entier : ");
-    scanf("%d",&x);
+    /*printf("****suppression d'un %cl%cment apr%cs une r%cf%crence
+    donn%c****\n",130,130,130,130,130,130); printf("Donner un entier : "); scanf("%d",&x);
     p=recherche_reference(L1,x);
     if(p)
     {
--- Semestre2/Cours-ASD-en-C/LinkedList/TDA/Liste-UNI-TDA.c
+++ Semestre2/Cours-ASD-en-C/LinkedList/TDA/Liste-UNI-TDA.c
@@ -1,19 +1,23 @@
 /* partie implementation */
-#include "liste.h"
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "liste.h"
 /******Fonction creer_liste*******/
-void creer_liste(struct liste* L) {
+void creer_liste(struct liste* L)
+{
     L->premier = NULL;
     L->dernier = NULL;
 }
 /******Fonction liste_vide*******/
-int liste_vide(struct liste L) {
+int liste_vide(struct liste L)
+{
     return L.premier == NULL;
 }
 /******Fonction inserer_avant_premier******/
-void inserer_avant_premier(struct liste* L, int x) {
+void inserer_avant_premier(struct liste* L, int x)
+{
     struct cellule* p;
     p          = (struct cellule*)malloc(sizeof(struct cellule));
     p->cle     = x;
@@ -23,14 +27,17 @@ void inserer_avant_premier(struct liste* L, int x) {
     L->premier = p;
 }
 /**Fonction Remplir 1 en utilisant inserer_avant_premier**/
-void remplir1(struct liste* L) {
+void remplir1(struct liste* L)
+{
     int      x;
     unsigned rep, i = 1;
-    do {
+    do
+    {
         printf("Donner un entier %d de la liste: ", i);
         scanf("%d", &x);
         inserer_avant_premier(L, x);
-        do {
+        do
+        {
             printf("Voulez vous inserer un autre entier ds la liste(1:oui,0:non): ");
             scanf("%u", &rep);
         } while (!(rep == 1 || rep == 0));
@@ -38,7 +45,8 @@ void remplir1(struct liste* L) {
     } while (rep);
 }
 /******Fonction inserer_apres_dernier******/
-void inserer_apres_dernier(struct liste* L, int x) {
+void inserer_apres_dernier(struct liste* L, int x)
+{
     struct cellule* p;
     p          = (struct cellule*)malloc(sizeof(struct cellule));
     p->cle     = x;
@@ -50,14 +58,17 @@ void inserer_apres_dernier(struct liste* L, int x) {
     L->dernier = p;
 }
 /**Fonction Remplir 1 en utilisant inserer_aprés_dernier**/
-void remplir2(struct liste* L) {
+void remplir2(struct liste* L)
+{
     int      x;
     unsigned rep, i = 1;
-    do {
+    do
+    {
         printf("Donner un entier %d de la liste: ", i);
         scanf("%d", &x);
         inserer_apres_dernier(L, x);
-        do {
+        do
+        {
             printf("Voulez vous inserer un autre entier ds la liste(1:oui,0:non): ");
             scanf("%u", &rep);
         } while (!(rep == 1 || rep == 0));
@@ -65,14 +76,17 @@ void remplir2(struct liste* L) {
     } while (rep);
 }
 /****Fonction TAILLE***/
-int taille(struct liste L) {
+int taille(struct liste L)
+{
     struct cellule* p;
     unsigned        t = 0;
     if (liste_vide(L))
         return (0);
-    else {
+    else
+    {
         p = L.premier;
-        while (p != NULL) {
+        while (p != NULL)
+        {
             p = p->suivant;
             t++;
         }
@@ -80,24 +94,28 @@ int taille(struct liste L) {
     }
 }
 /**** Fonction recherche_référence****/
-struct cellule* recherche_reference(struct liste L, int x) {
+struct cellule* recherche_reference(struct liste L, int x)
+{
     struct cellule* p = L.premier;
     while (p && p->cle != x)
         p = p->suivant;
     return p;
 }
 /***Fonction affiche***/
-void affiche(struct liste L) {
+void affiche(struct liste L)
+{
     struct cellule* q = L.premier;
     assert(!(liste_vide(L)));
-    while (q != NULL) {
+    while (q != NULL)
+    {
         printf("%d\t", q->cle);
         q = q->suivant;
     }
     printf("\n");
 }
 /****Fonction insertion après élément référencée*****/
-void inserer_apres_reference(struct cellule* p, int x) {
+void inserer_apres_reference(struct cellule* p, int x)
+{
     struct cellule* q;
     q          = (struct cellule*)malloc(sizeof(struct cellule));
     q->cle     = x;
@@ -105,7 +123,8 @@ void inserer_apres_reference(struct cellule* p, int x) {
     p->suivant = q;
 }
 /****Fonction insertion avant élément référencée*****/
-void inserer_avant_reference(struct cellule* p, int x) {
+void inserer_avant_reference(struct cellule* p, int x)
+{
     struct cellule* q;
     q          = (struct cellule*)malloc(sizeof(struct cellule));
     *q         = *p;
@@ -113,7 +132,8 @@ void inserer_avant_reference(struct cellule* p, int x) {
     p->suivant = q;
 }
 /*****Fonction suppression du premier élément****/
-void supprimer_premier(struct liste* L) {
+void supprimer_premier(struct liste* L)
+{
     struct cellule* p;
     assert(!(liste_vide(*L)));
     p          = L->premier;
@@ -124,12 +144,14 @@ void supprimer_premier(struct liste* L) {
         L->dernier = NULL;
 }
 /*****Fonction suppression du dernier élément****/
-void supprimer_dernier(struct liste* L) {
+void supprimer_dernier(struct liste* L)
+{
     struct cellule* p; /*pour parcourir ll */
     assert(!(liste_vide(*L)));
     if (L->premier == L->dernier) /* liste formé d’un seul élément*/
         supprimer_premier(L);
-    else {
+    else
+    {
         p = L->premier;
         while (p->suivant != L->dernier)
             p = p->suivant;
@@ -139,21 +161,24 @@ void supprimer_dernier(struct liste* L) {
     }
 }
 /*****Fonction suppression d’un élément référencée******/
-void supprimer_element_reference(struct cellule* p) {
+void supprimer_element_reference(struct cellule* p)
+{
     struct cellule* q;
     q  = p->suivant;
     *p = *q;
     free(q);
 }
 /*****Fonction supprimer_apres_reference******/
-void supprimer_apres_reference(struct cellule* p) {
+void supprimer_apres_reference(struct cellule* p)
+{
     struct cellule* q;
     q          = p->suivant;
     p->suivant = q->suivant;
     free(q);
 }
 
-void supprimer_avant_reference(struct liste* l, struct cellule* p) {
+void supprimer_avant_reference(struct liste* l, struct cellule* p)
+{
     struct cellule* q = l->premier;
     while (q->suivant != p)
         q = q->suivant;
